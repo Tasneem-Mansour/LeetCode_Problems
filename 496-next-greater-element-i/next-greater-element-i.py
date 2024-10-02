d6a6b6a1954bd01997c15cb61b9fdfc9d3354c13
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-03 00:21:00 +0300

    Time: 87 ms (12.18%) | Memory: 16.8 MB (29.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,20 +1,35 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        out=[]
-        for i in range(0,len(nums1)):
-            if(nums2.index(nums1[i])==len(nums2)-1):
-                out.append(-1)
-            else:
-                j=nums2.index(nums1[i])+1
-                while(1):
-                    if(nums1[i]<nums2[j]):
-                        out.append(nums2[j])
-                        break
-                    if(j==len(nums2)-1):
-                        out.append(-1)
-                        break
-                    j+=1
-        return out
+        #### Solution1 -> solved 12/16 testcases####
+        # returnList = []
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j]:
+        #             if j == len(nums2)-1:
+        #                 returnList.append(-1)
+        #             else:
+        #                 while True:
+        #                     if j == len(nums2)-1:
+        #                         returnList.append(-1)
+        #                         break
+        #                     if nums2[j+1] > nums2[j]:
+        #                         returnList.append(nums2[j+1])
+        #                         break
+        #                     j += 1
 
+        # return returnList
+
+        ####Solution 2####
+        res = [-1] * len(nums1)
+
+        for i in range (len(nums2)):
+            if nums2[i] in nums1:
+                index = nums1.index(nums2[i])
+                for j in range(i+1, len(nums2)):
+                    if nums2[j] > nums2[i]:
+                        res[index] = nums2[j]
+                        break
+
+        return res                                
 
         
