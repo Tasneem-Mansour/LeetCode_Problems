012a48aa6c776144ec0e070880a7b9b6596001e8
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-03 01:02:28 +0300

    Time: 43 ms (83.37%) | Memory: 16.9 MB (29.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -20,16 +20,38 @@ class Solution:
         # return returnList
 
         ####Solution 2####
+        # res = [-1] * len(nums1)
+
+        # for i in range (len(nums2)):
+        #     if nums2[i] in nums1:
+        #         index = nums1.index(nums2[i])
+        #         for j in range(i+1, len(nums2)):
+        #             if nums2[j] > nums2[i]:
+        #                 res[index] = nums2[j]
+        #                 break
+        # return res                                
+
+        ####Solution 3####
+
         res = [-1] * len(nums1)
+        hashIndxNums1 = {}
+        for i, n in enumerate(nums1):
+            hashIndxNums1[n] = i
+        stack = []
+        for i in range(len(nums2)):
+            
+            while stack and nums2[i] > stack[-1]:
+                value = stack.pop()
+                res[hashIndxNums1[value]] = nums2[i]
 
-        for i in range (len(nums2)):
-            if nums2[i] in nums1:
-                index = nums1.index(nums2[i])
-                for j in range(i+1, len(nums2)):
-                    if nums2[j] > nums2[i]:
-                        res[index] = nums2[j]
-                        break
+            if nums2[i] in hashIndxNums1:
+                stack.append(nums2[i])
 
-        return res                                
+            # 1 in nums1? yes
+            # next num is greater?  no -> append in stack 
+                                #   yes -> pop all from stack and aoppend the num in its index in res
+
+        return res
+
 
         
