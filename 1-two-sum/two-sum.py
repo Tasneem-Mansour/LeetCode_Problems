c09cf788959c0a9b52ace74a3b2f132562d21222
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-06 19:05:59 +0300

    Time: 62 ms (51.81%) | Memory: 17.7 MB (35.82%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        ####Solution1####  not very good approach
+        ####Solution1####  not very good approach   O(n^2)
         # for i in range(len(nums)):
         #     for j in range(1, len(nums)):
-        #         if nums[i]+nums[j] == target and i!=j:  
+        #         if nums[i]   +nums[j] == target and i!=j:  
         #             return i, j
 
 
@@ -16,16 +15,26 @@ class Solution:
 
 
 
-        ####Solution3####
-        hashmap = {}                #value -> index
+        ####Solution 3####
+        # hashmap = {}                 #value -> index
 
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i        
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]] = i        
 
+        # for i in range(len(nums)):
+        #     complement = target - nums[i]
+        #     if complement in hashmap and i != hashmap[complement]:
+        #         return i, hashmap[complement] 
+
+        ####Solution 4####  O(n)
+        hashMap = {}
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in hashmap and i != hashmap[complement]:
-                return i, hashmap[complement] 
-            
+            if complement in hashMap:
+                return [hashMap[complement], i]
+            hashMap[nums[i]] = i
+        return 
+    
+
 
         
