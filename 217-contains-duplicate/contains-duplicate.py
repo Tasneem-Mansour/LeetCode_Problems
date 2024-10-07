77922206b977610405a63cea5d37024fbd8028f1
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-07 23:16:31 +0300

    Time: 430 ms (28.95%) | Memory: 32.1 MB (25.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,30 +1,28 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        #####Solution1#####
+        setOfNums = set()
+        for n in nums:
+            if n in setOfNums:
+                return True
+            else:
+                setOfNums.add(n)
+        return False
+
+        #####Solution2#####
+        # setOfNums = set(nums)
+
+        # if len(nums)==len(setOfNums):
+        #     return False
+        # else:
+        #     return True
+
+        ####Solution 3####
         # hashMap = {}
         # for i in range(len(nums)):
         #     hashMap[nums[i]] = 1 + hashMap.get(nums[i],0)
         #     if hashMap[nums[i]] > 1:
         #         return True
         # return
-
-        #####Solution1#####
-        # setOfNums = set()
-
-        # for n in nums:
-        #     if n in setOfNums:
-        #         return True
-        #     else:
-        #         setOfNums.add(n)
-
-        # return False
-
-        #####Solution2#####
-        setOfNums = set(nums)
-
-        if len(nums)==len(setOfNums):
-            return False
-        else:
-            return True
-
 
         
