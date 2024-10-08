4c891851b99735d9a7b253f5de7335009290b422
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-09 01:45:06 +0300

    Time: 462 ms (55.74%) | Memory: 29.9 MB (54.10%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,11 +1,25 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        ####Solution 1####  O(n)
         hashMap = {}
         for i in range(len(nums)):
             if nums[i] in hashMap and (i - hashMap[nums[i]]) <= k:
                 return True
-            
-            hashMap[nums[i]] = i
-            
+            hashMap[nums[i]] = i   
         return
+
+        ####Solution 2 - sliding window####  O(n)
+
+        window = set()   
+        L = 0          
+
+        for R in range(len(nums)):
+            if R-L > k:   
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return
+
         
