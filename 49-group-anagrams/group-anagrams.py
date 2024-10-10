5e229059de1e62fd03d72e53f7ac87d750ace0aa
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-10 10:58:17 +0300

    Time: 104 ms (13.71%) | Memory: 24.1 MB (10.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ####Solution 1 (doesnt work)#### time limit exceed
+        ####Solution 1 (doesnt work)#### time limit exceeded in some TCs
         # hashMap = {}           
-        # res = []
         # for i in range(len(strs)):
         #     word = "".join(sorted(strs[i]))
         #     if word in hashMap:
@@ -10,23 +9,33 @@ class Solution:
         #     else:
         #         hashMap[word] = [strs[i]]
         #     print (hashMap)
-        # for key, value in hashMap.items():
-        #     res.append(value)
-        
-        # return res
+        # return hashMap.values()
                     
-        ####Solution 2 ####    
+        ####Solution 2 ####    O(m*nlogn)
         # "aet": ["eat", "tea", "ate"],
         # "ant": ["tan", "nat"],
         # "abt": ["bat"]
 
+        # hashMap = defaultdict(list)
+        # for i in range(len(strs)):
+        #     sortedWord = "".join(sorted(strs[i]))
+        #     hashMap[sortedWord].append(strs[i])
+        # print(hashMap)
+        # return hashMap.values()
+
+        ####Solution 3####       O(m*n)
         hashMap = defaultdict(list)
-        for i in range(len(strs)):
-            sortedWord = "".join(sorted(strs[i]))
+        for word in strs:
+            count = [0] * 26    #a -> z
+            for letter in word:
+                count[ord(letter) - ord("a")] += 1
+            hashMap[tuple(count)].append(word)
 
-            hashMap[sortedWord].append(strs[i])
-
+        print(hashMap)
         return hashMap.values()
+            
+        
+
 
 
 
