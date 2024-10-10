0d96bbd4ce713b25b16e38d759d14ef9b22fbf46
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-10 17:04:36 +0300

    Time: 119 ms (28.24%) | Memory: 17.8 MB (61.72%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,17 +1,37 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # sorting O(nlogn) then binary search O(logn) -> O(nlogn)
+        ####Solution 1 #### not very efficient time comp = O(nlogn), space comp = O(n)
+        # nums.sort()         #[0,1]
+        # target = []         #[0,1,2]
 
-        nums.sort()         #[0,1]
+        # for i in range(0, len(nums)+1):
+        #     target.append(i)
+
+        # while len(nums) != len(target):
+        #     nums.append(0)
+
+        # for i in range(len(nums)):
+        #     if nums[i] != target[i]:
+        #         return target[i]
+
+        ####Solution 2 #### we can do better time comp = O(n), space comp = O(n)
+        # n = len(nums)
+        # missingNum = [0] * n+1          #[1,1,0,1]
+
+        # for i in nums:
+        #     missingNum[i] = 1
+
+        # for i in range(len(missingNum)):
+        #     if missingNum[i] == 0:
+        #         return i
+        # return 0
         
-        target = []         #[0,1,2]
+        ####Solution 3 #### time comp = O(n), space comp = O(1)
+        n = len(nums)
+        res = 0
+        for i in range(0, n+1):
+            res ^= i
+        for num in nums:
+            res ^= num
 
-        for i in range(0, len(nums)+1):
-            target.append(i)
-
-        while len(nums) != len(target):
-            nums.append(0)
-
-        for i in range(len(nums)):
-            if nums[i] != target[i]:
-                return target[i]
+        return res
