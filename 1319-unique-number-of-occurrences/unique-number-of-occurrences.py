fc786117b484fb3e9aa0035bffd4e502c376c60f
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-10 15:46:44 +0300

    Time: 45 ms (27.71%) | Memory: 16.6 MB (89.29%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -3,9 +3,8 @@ class Solution:
         Occurrences = {}        #{1:3, 2:2, 3:1}
         for i in range(len(arr)):
             Occurrences[arr[i]] = 1 + Occurrences.get(arr[i],0)
-        unique = set()
-        for key in Occurrences:
-            unique.add(Occurrences[key])
-        if len(unique) == len(Occurrences):
-            return True
-        return False
+
+        unique = set(Occurrences.values())
+
+        return len(unique) == len(Occurrences)
+  
