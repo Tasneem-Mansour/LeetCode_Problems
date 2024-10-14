24b8af6eab9a225642cb14ae830064248cfe4ab3
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-14 17:50:13 +0300

    Time: 33 ms (91.43%) | Memory: 17.6 MB (85.82%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -9,20 +9,36 @@ class Solution:
         if not root:
             return 0
         #BFS
-        q = deque([root])
-        level = 0
-        while q:        #9 20
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
+        # q = deque([root])
+        # level = 0
+        # while q:        #9 20
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level += 1
+        # return level
 
+        #Recursive DFS - Inorder - left, node, right  O(n)
+        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+        #Iterative DFS - preorder - node, left, right
+        stack = [[root, 1]]     #[, [20,2], [9,2]]
+        res = 0
+        while stack:
+            node, level = stack.pop()
+            # res = max(res, level)
+            # if node.right:
+            #     stack.append([node.right, level+1])
+            # if node.left:
+            #     stack.append([node.left, level+1])
 
-                    
+            if node:        #node can ne null as its children will be added even if null
+                res = max(res, level)
+                stack.append([node.right, level+1])
+                stack.append([node.left, level+1])
 
+        return res
         
