84e54818566a48f2b17459b936e9c22744619dc3
Tasneem Mansour <114835445+Tasneem-Mansour@users.noreply.github.com>
2024-10-15 01:05:28 +0300

    Time: 34 ms (72.98%) | Memory: 16.7 MB (52.72%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -9,18 +9,27 @@ class Solution:
         if not root or (not root.left and not root.right):
             return 0
         #BFS
-        q = deque([root])
-        sum = 0
-        while q:        
-            node = q.popleft()         
-            if node:
-                if node.left and not node.left.left and not node.left.right:
-                    sum += node.left.val
+        # q = deque([root])
+        # sum = 0
+        # while q:        
+        #     node = q.popleft()         
+        #     if node:
+        #         if node.left and not node.left.left and not node.left.right:
+        #             sum += node.left.val
                 
-                q.append(node.left)
-                q.append(node.right)
+        #         q.append(node.left)
+        #         q.append(node.right)
+        # return sum        
 
-        return sum        
-            
-
+        #DFS
+        self.sum = 0
+        def dfs(node):
+            if node:    
+                if node.left and not node.left.left and not node.left.right:
+                    self.sum += node.left.val  #9+15
+                dfs(node.left)
+                dfs(node.right)
+        dfs(root)
+        return self.sum
+         
         
